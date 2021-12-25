c83e7f420f17fec89e802c6833675eb32041af7e
hridoy-orangetoolz <hridoy@orangetoolz.com>
2021-12-25 15:18:40 +0600

    feat: API call to frontend

--- frontend/src/ApiEndPoints.js
+++ frontend/src/ApiEndPoints.js
@@ -1,4 +1,7 @@
 export const APiEndPoints = {
-  API_URL: "http://127.0.0.1:8000/api/person/",
   PRODUCT_URL: "http://127.0.0.1:8000/api/product/",
+  VARIANT_URL: "http://127.0.0.1:8000/api/variant/",
+  PRODUCT_VARIANT_URL: "http://127.0.0.1:8000/api/product-variant/",
+  PRODUCT_VARIANT_PRICE_URL: "http://127.0.0.1:8000/api/product-variant-price/",
+  PRODUCT_IMAGE_URL: "http://127.0.0.1:8000/api/product-image/",
 };
--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,42 +1,49 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { APiEndPoints } from "../../ApiEndPoints";
-import PersonInformation from "../PersonInfo/PersonInformation";
-import ProductInformation from "../ProductInfo/Information";
 
 const Home = () => {
-  const [personInfo, setPersonInfo] = useState([]);
-  const [productInfo, setProductInfo] = useState([]);
+  const [productInfo, setProductInfo] = useState({
+    products: [],
+    variants: [],
+    productVeriants: [],
+    productVariantPrices: [],
+    productImages: [],
+  });
 
   useEffect(() => {
-    getAllPersonInfo();
-    getAllProductInfo();
+    const fetchData = async () => {
+      const product = await axios(`${APiEndPoints.PRODUCT_URL}`);
+      const variant = await axios(`${APiEndPoints.VARIANT_URL}`);
+      const productVariant = await axios(`${APiEndPoints.PRODUCT_VARIANT_URL}`);
+      const productVariantPrice = await axios(
+        `${APiEndPoints.PRODUCT_VARIANT_PRICE_URL}`
+      );
+      const productImage = await axios(`${APiEndPoints.PRODUCT_IMAGE_URL}`);
+
+      setProductInfo({
+        products: [...productInfo.products, ...product.data],
+        variants: [...productInfo.variants, ...variant.data],
+        productVeriants: [
+          ...productInfo.productVeriants,
+          ...productVariant.data,
+        ],
+        productVariantPrices: [
+          ...productInfo.productVariantPrices,
+          ...productVariantPrice.data,
+        ],
+        productImages: [
+          ...productInfo.productVariantPrices,
+          ...productImage.data,
+        ],
+      });
+    };
+
+    fetchData();
   }, []);
-
-  const getAllPersonInfo = () => {
-    axios
-      .get(APiEndPoints.API_URL)
-      .then((response) => {
-        const allInfo = response.data;
-        setPersonInfo(allInfo);
-      })
-      .catch((error) => console.error(`Error: ${error}`));
-  };
-  const getAllProductInfo = () => {
-    axios
-      .get(APiEndPoints.PRODUCT_URL)
-      .then((response) => {
-        const productInfo = response.data;
-        setProductInfo([...productInfo, productInfo]);
-      })
-      .catch((error) => console.error(`Error: ${error}`));
-  };
-
   return (
     <div className="main__container">
       <h1>Coding Test - Mediusware Ltd.</h1>
-      <PersonInformation personInfo={personInfo} />
-      <ProductInformation productInfo={productInfo} />
     </div>
   );
 };
